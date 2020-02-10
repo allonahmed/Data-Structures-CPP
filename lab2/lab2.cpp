f93d56f3901c5b89e5de3f5a6ab7b56d0cbde91e
allonahmed <allonahmed@mec.science>
2020-02-10 09:18:45 -0500

    fixing folder arrangement

---
+++ lab2/lab2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Circle.h"
+
+
+int main() {
+  Circle one(3);
+  std::cout << one;
+  
+}
---
+++ lab3/shape.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Shape.h"
+using namespace std;
+
+int main() {
+  Square one(5);
+  cout << one << endl;
+}
